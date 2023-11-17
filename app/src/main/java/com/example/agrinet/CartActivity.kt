952b853cb3790c0022dade67c6fc1ff41aeeff58
oliver-sys <jamesoliverg@outlook.com>
2023-11-17 18:38:58 +0800

    added cart and profile

--- app/src/main/java/com/example/agrinet/CartActivity.kt
+++ app/src/main/java/com/example/agrinet/CartActivity.kt
@@ -1,11 +1,28 @@
 package com.example.agrinet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cart)
+
+        val homeButton = findViewById<ImageView>(R.id.homeButton)
+        val profileButton = findViewById<ImageView>(R.id.profileButton)
+
+
+        profileButton.setOnClickListener{
+            val intent = Intent( this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        homeButton.setOnClickListener{
+            val intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/agrinet/ProfileActivity.kt
+++ app/src/main/java/com/example/agrinet/ProfileActivity.kt
@@ -1,11 +1,28 @@
 package com.example.agrinet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
+
+        val cartButton = findViewById<ImageView>(R.id.cartButton)
+        val homeButton = findViewById<ImageView>(R.id.homeButton)
+
+
+        cartButton.setOnClickListener{
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
+
+        homeButton.setOnClickListener{
+            val intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
