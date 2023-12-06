d77af9055143793c8d1d8db609b4add31cb1c941
oliver-sys <jamesoliverg@outlook.com>
2023-12-07 04:32:59 +0800

    database

---
+++ app/src/main/java/com/example/agrinet/RegisterActivity.kt
@@ -0,0 +1,90 @@
+package com.example.agrinet
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.content.Intent
+import android.widget.EditText
+import android.widget.TextView
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+        // Initialize Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+
+        // Initialize EditTexts
+        editEmail = findViewById(R.id.editEmail)
+        editPassword = findViewById(R.id.editPassword)
+
+        val loginButton = findViewById<TextView>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        // Initialize Button
+        val registerButton = findViewById<ConstraintLayout>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val email = editEmail.text?.toString()
+            val password = editPassword.text?.toString()
+
+            if (!email.isNullOrBlank() && !password.isNullOrBlank()) {
+                // Call function to register user with email and password
+                registerUser(email, password)
+            } else {
+                // Handle empty email or password
+                Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Rest of your onCreate logic...
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration successful
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // Registration failed
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            // User is signed in
+            // Redirect to the main activity, home screen, or perform any other action
+
+            // Example: Start a new activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Finish the current activity to prevent going back to the registration screen
+            finish()
+        } else {
+            // User registration failed or user is not signed in
+            // You can update the UI to display an error message or take appropriate actions
+            // Example: Display an error message in a TextView
+            Toast.makeText(this, "User registration failed. Please try again.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
