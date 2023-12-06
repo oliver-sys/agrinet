2bf96897cc9037a46434c515590c973a890d9e54
oliver-sys <jamesoliverg@outlook.com>
2023-12-07 04:32:40 +0800

    login and register

--- app/src/main/java/com/example/agrinet/LoginActivity.kt
+++ app/src/main/java/com/example/agrinet/LoginActivity.kt
@@ -2,23 +2,83 @@ package com.example.agrinet
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val loginButton = findViewById<ConstraintLayout>(R.id.loginButton)
+        // Initialize Firebase Authentication
+        auth = FirebaseAuth.getInstance()
 
+        // Initialize EditTexts
+        editEmail = findViewById(R.id.editEmail)
+        editPassword = findViewById(R.id.editPassword)
+
+        val loginButton = findViewById<ConstraintLayout>(R.id.loginButton)
+        val registerButton = findViewById<TextView>(R.id.registerButton)
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val email = editEmail.text?.toString()
+            val password = editPassword.text?.toString()
+
+            if (!email.isNullOrBlank() && !password.isNullOrBlank()) {
+                // Call function to authenticate user
+                authenticateUser(email, password)
+            } else {
+                // Handle empty email or password
+                Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
         }
 
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
+    private fun authenticateUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Authentication successful
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // Authentication failed
+                    Toast.makeText(this, "Login failed. Please check your credentials.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            // User is signed in
+            // Redirect to the main activity, home screen, or perform any other action
+
+            // Example: Start a new activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Finish the current activity to prevent going back to the login screen
+            finish()
+        } else {
+            // User authentication failed or user is not signed in
+            // Display an error toast message
+            Toast.makeText(this, "Login failed. Please check your credentials.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/agrinet/ProfileActivity.kt
+++ app/src/main/java/com/example/agrinet/ProfileActivity.kt
@@ -3,32 +3,53 @@ package com.example.agrinet
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
 
+        // Initialize Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+
         val cartButton = findViewById<ImageView>(R.id.cartButton)
         val homeButton = findViewById<ImageView>(R.id.homeButton)
         val logoutButton = findViewById<ConstraintLayout>(R.id.logoutButton)
+        val userNameTextView = findViewById<TextView>(R.id.userName)
 
-        cartButton.setOnClickListener{
+        // Set the email of the currently logged-in user to the userName TextView
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            val userEmail = currentUser.email
+            userNameTextView.text = userEmail
+        }
+
+        cartButton.setOnClickListener {
             val intent = Intent(this, CartActivity::class.java)
             startActivity(intent)
         }
 
-        homeButton.setOnClickListener{
-            val intent = Intent( this, MainActivity::class.java)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        logoutButton.setOnClickListener{
-            val intent = Intent( this, LoginActivity::class.java)
-            startActivity(intent)
-        }
+        logoutButton.setOnClickListener {
+            // Sign out the user
+            auth.signOut()
 
+            // Redirect to the login page after logging out
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            // Finish the current activity to prevent going back to the profile screen
+            finish()
+        }
     }
 }
