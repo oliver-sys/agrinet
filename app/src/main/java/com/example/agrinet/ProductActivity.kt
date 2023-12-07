4b40806552de533777b741b66fbc8f662c1a341c
oliver-sys <jamesoliverg@outlook.com>
2023-12-07 10:35:37 +0800

    added products

--- app/src/main/java/com/example/agrinet/ProductActivity.kt
+++ app/src/main/java/com/example/agrinet/ProductActivity.kt
@@ -3,7 +3,12 @@ package com.example.agrinet
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +16,114 @@ class ProductActivity : AppCompatActivity() {
         setContentView(R.layout.product)
 
         val backButton = findViewById<ImageView>(R.id.backButton)
+        val product1 = findViewById<TextView>(R.id.t1)
+        val product2 = findViewById<TextView>(R.id.t2)
+        val product3 = findViewById<TextView>(R.id.t3)
+        val product4 = findViewById<TextView>(R.id.t4)
+        val product5 = findViewById<TextView>(R.id.t5)
+        val product6 = findViewById<TextView>(R.id.t6)
+        val product7 = findViewById<TextView>(R.id.t7)
+        val product8 = findViewById<TextView>(R.id.t8)
+        val product9 = findViewById<TextView>(R.id.t9)
+        val product10 = findViewById<TextView>(R.id.t10)
 
-        backButton.setOnClickListener{
-            val intent = Intent( this, ShopActivity::class.java)
-            startActivity(intent)
+        val productPrice1 = findViewById<TextView>(R.id.p1)
+        val productPrice2 = findViewById<TextView>(R.id.p2)
+        val productPrice3 = findViewById<TextView>(R.id.p3)
+        val productPrice4 = findViewById<TextView>(R.id.p4)
+        val productPrice5 = findViewById<TextView>(R.id.p5)
+        val productPrice6 = findViewById<TextView>(R.id.p6)
+        val productPrice7 = findViewById<TextView>(R.id.p7)
+        val productPrice8 = findViewById<TextView>(R.id.p8)
+        val productPrice9 = findViewById<TextView>(R.id.p9)
+        val productPrice10 = findViewById<TextView>(R.id.p10)
 
+        val productButton1 = findViewById<ConstraintLayout>(R.id.pButton1)
+        val productButton2 = findViewById<ConstraintLayout>(R.id.pButton2)
+        val productButton3 = findViewById<ConstraintLayout>(R.id.pButton3)
+        val productButton4 = findViewById<ConstraintLayout>(R.id.pButton4)
+        val productButton5 = findViewById<ConstraintLayout>(R.id.pButton5)
+        val productButton6 = findViewById<ConstraintLayout>(R.id.pButton6)
+        val productButton7 = findViewById<ConstraintLayout>(R.id.pButton7)
+        val productButton8 = findViewById<ConstraintLayout>(R.id.pButton8)
+        val productButton9 = findViewById<ConstraintLayout>(R.id.pButton9)
+        val productButton10 = findViewById<ConstraintLayout>(R.id.pButton10)
+
+        // Initialize Firebase
+        val database = FirebaseDatabase.getInstance()
+        val databaseRef = database.reference.child("sellers")
+
+        // Attach a ValueEventListener to fetch data from the sellers node
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (sellerSnapshot in dataSnapshot.children) {
+                    // Assuming the structure is sellers -> seller1 -> products -> product1, product2, ...
+                    for (productSnapshot in sellerSnapshot.child("products").children) {
+                        // Fetch product name and price
+                        val productName = productSnapshot.child("name").getValue(String::class.java)
+                        val productPrice = productSnapshot.child("pricePerKilo").getValue(Double::class.java)
+
+                        // Determine which TextViews to update based on product position
+                        // Assuming the products are sorted in the same order as your TextViews
+                        when (productSnapshot.key) {
+                            "product1" -> {
+                                product1.text = productName
+                                productPrice1.text = "$productPrice"
+                            }
+                            "product2" -> {
+                                product2.text = productName
+                                productPrice2.text = "$productPrice"
+                            }
+                            // Continue this pattern for the other products
+                            "product3" -> {
+                                product3.text = productName
+                                productPrice3.text = "$productPrice"
+                            }
+                            "product4" -> {
+                                product4.text = productName
+                                productPrice4.text = "$productPrice"
+                            }
+                            "product5" -> {
+                                product5.text = productName
+                                productPrice5.text = "$productPrice"
+                            }
+                            "product6" -> {
+                                product6.text = productName
+                                productPrice6.text = "$productPrice"
+                            }
+                            "product7" -> {
+                                product7.text = productName
+                                productPrice7.text = "$productPrice"
+                            }
+                            "product8" -> {
+                                product8.text = productName
+                                productPrice8.text = "$productPrice"
+                            }
+                            "product9" -> {
+                                product9.text = productName
+                                productPrice9.text = "$productPrice"
+                            }
+                            "product10" -> {
+                                product10.text = productName
+                                productPrice10.text = "$productPrice"
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        }
+
+        // Add the ValueEventListener to the database reference
+        databaseRef.addValueEventListener(valueEventListener)
+
+        backButton.setOnClickListener {
+            // Navigate back to the ShopActivity
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/agrinet/ShopActivity.kt
+++ app/src/main/java/com/example/agrinet/ShopActivity.kt
@@ -3,8 +3,11 @@ package com.example.agrinet
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+
+import com.google.firebase.database.FirebaseDatabase
 
 class ShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,16 +15,96 @@ class ShopActivity : AppCompatActivity() {
         setContentView(R.layout.shop)
 
         val homeButton = findViewById<ImageView>(R.id.homeButton)
-        val productButton = findViewById<ConstraintLayout>(R.id.shopButton2)
+        val sellerProduct1 = findViewById<ConstraintLayout>(R.id.sellerProduct1)
+        val sellerProduct2 = findViewById<ConstraintLayout>(R.id.sellerProduct2)
+        val sellerProduct3 = findViewById<ConstraintLayout>(R.id.sellerProduct3)
+        val sellerProduct4 = findViewById<ConstraintLayout>(R.id.sellerProduct4)
+        val sellerProduct5 = findViewById<ConstraintLayout>(R.id.sellerProduct5)
 
-        homeButton.setOnClickListener{
-            val intent = Intent( this, MainActivity::class.java)
-            startActivity(intent)
+        val sellerName1 = findViewById<TextView>(R.id.sellerName1)
+        val sellerName2 = findViewById<TextView>(R.id.sellerName2)
+        val sellerName3 = findViewById<TextView>(R.id.sellerName3)
+        val sellerName4 = findViewById<TextView>(R.id.sellerName4)
+        val sellerName5 = findViewById<TextView>(R.id.sellerName5)
+        val address1 = findViewById<TextView>(R.id.address1)
+        val address2 = findViewById<TextView>(R.id.address2)
+        val address3 = findViewById<TextView>(R.id.address3)
+        val address4 = findViewById<TextView>(R.id.address4)
+        val address5 = findViewById<TextView>(R.id.address5)
+
+        // Initialize Firebase
+        val database = FirebaseDatabase.getInstance()
+        val databaseRef = database.reference
+
+        // Assuming your sellers' data is stored in a node named "sellers"
+        val sellersRef = databaseRef.child("sellers")
+
+        // Fetch data for each seller
+        for (i in 1..5) {
+            val currentSellerRef = sellersRef.child("seller$i")
+
+            currentSellerRef.get().addOnSuccessListener { snapshot ->
+                if (snapshot.exists()) {
+                    val sellerName = snapshot.child("name").getValue(String::class.java)
+                    val sellerAddress = snapshot.child("address").getValue(String::class.java)
+
+                    // Assign data to TextViews
+                    when (i) {
+                        1 -> {
+                            sellerName1.text = sellerName
+                            address1.text = sellerAddress
+                        }
+                        2 -> {
+                            sellerName2.text = sellerName
+                            address2.text = sellerAddress
+                        }
+                        3 -> {
+                            sellerName3.text = sellerName
+                            address3.text = sellerAddress
+                        }
+                        4 -> {
+                            sellerName4.text = sellerName
+                            address4.text = sellerAddress
+                        }
+                        5 -> {
+                            sellerName5.text = sellerName
+                            address5.text = sellerAddress
+                        }
+                    }
+                } else {
+                    //
+                }
+            }
         }
 
-        productButton.setOnClickListener{
-            val intent = Intent ( this, ProductActivity::class.java)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+        sellerProduct1.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+
+        }
+        sellerProduct2.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+
+        }
+        sellerProduct3.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+
+        }
+        sellerProduct4.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+
+        }
+        sellerProduct5.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+
         }
 
     }
